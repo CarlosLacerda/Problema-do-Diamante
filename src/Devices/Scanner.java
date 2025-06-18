1018cb0d4478a965bb2e62cf52c378ec64125efc
CarlosLacerda <carlosalacerda01@gmail.com>
2025-06-18 16:36:31 -0300

    Corrigido - FUNCIONANDO

--- src/Devices/Scanner.java
+++ src/Devices/Scanner.java
@@ -1,18 +1,7 @@
 package Devices;
 
-public class Scanner extends Device {
-
-	public Scanner(String serialNumber) {
-		super(serialNumber);
-	}
+public interface Scanner {
 	
-	@Override
-	public void processDoc(String doc) {
-		System.out.println("Scanner processing: " + doc);
-	}
-	
-	public String scan() {
-		return "Scanner content";
-	}
+	String scan();
 
 }
